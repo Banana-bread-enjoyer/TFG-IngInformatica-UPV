072fe325b5d066fa5b7d5b920e029ac79c9f8cd4
MartaRamalho <martaramalho512@gmail.com>
2024-04-12 00:18:47 +0200

    extraxt more info from anexo1

--- extraerAnexo1/extraerAnexo1_3.py
+++ extraerAnexo1/extraerAnexo1_3.py
@@ -9,7 +9,7 @@ def guardar_html_en_archivo(html, nombre_archivo="respuesta.html"):
         archivo.write(html)
 
 
-doc = fitz.open("Anexo1Ejemplo_2.pdf")  # open a document
+doc = fitz.open("Anexo1Ejemplo.pdf")  # open a document
 lines = []
 for page in doc:
     lines.extend(page.get_text().split("\n"))
@@ -40,13 +40,13 @@ def extraer_texto_sección(i, dato):
 
 def extraer_valor_monetario(i):
     pattern = re.compile(r"\d{1,3}(.\d{3})*,\d+")
-    block = lines[i]
-    match = re.search(pattern, block)
+    linea_actual = lines[i + 1]
+    match = re.search(pattern, linea_actual)
     if match:
         return match.group(0)
     else:
-        block = blocks[i + 1][4]
-        match = re.search(pattern, block)
+        linea = lines[i + 2]
+        match = re.search(pattern, linea)
         if match:
             return match.group(0)
     return None
@@ -69,6 +69,37 @@ def remove_stopwords(text):
     return new_text
 
 
+# Function to extract categories, groups, and subgroups
+def extract_info(input_string):
+    category_pattern = r"Categoría\:?\s* (\d+)\.?"
+    group_pattern = r"Grupo\:?\s* ([A-Z])\.? ([\w\s]+)\."
+    subgroup_pattern = r"Subgrupo\:?\s* (\d+)\.? (.+)\."
+    # Initialize an empty dictionary to store the results
+    result = {}
+
+    # Extract category
+    category_match = re.search(category_pattern, input_string, flags=re.IGNORECASE)
+    if category_match:
+        category_number = category_match.group(1)
+        result["Categoría"] = int(category_number)
+
+    # Extract group
+    group_match = re.search(group_pattern, input_string, flags=re.IGNORECASE)
+    if group_match:
+        group_letter = group_match.group(1)
+        group_description = group_match.group(2)
+        result["Grupo"] = f"{group_letter}, {group_description}"
+
+    # Extract subgroup
+    subgroup_match = re.search(subgroup_pattern, input_string, flags=re.IGNORECASE)
+    if subgroup_match:
+        subgroup_number = subgroup_match.group(1)
+        subgroup_description = subgroup_match.group(2)
+        result["Subgrupo"] = f"{subgroup_number}, {subgroup_description}"
+
+    return result
+
+
 df = pd.read_excel(
     "../NOMBRE_DATOS.xlsx", sheet_name="Hoja1"
 )  # can also index sheet by name or fetch all sheets
@@ -77,7 +108,7 @@ mylist = [x.upper().strip() for x in mylist]
 myDict = {key: None for key in mylist}
 
 myDict2 = {
-    "¿SE HA EXIGIDO ALGUNA CLASIFICACIÓN?": "Texto",
+    "CLASIFICACIÓN": "Texto",
     "MODIFICACIONES PREVISTAS": "Valor",
     "PRÓRROGAS": "Valor",
     "REVISIÓN DE PRECIOS": "Valor",
@@ -89,37 +120,54 @@ myDict2 = {
     "¿REVISIÓN DE PRECIOS?": "SN",
     "POSIBILIDAD DE PRORROGAR EL CONTRATO": "SN",
     "PLAZO MÁXIMO DE LAS PRÓRROGAS": "Texto",
-    "UNIDAD ENCARGADA DEL SEGUIMIENTO Y EJECUCIÓN DEL CONTRATO": "Texto",
-    "CONSIDERACIÓN COMO INFRACCIÓN GRAVE DEL INCUMPLIMIENTO DE LAS CONDICIONES ESPECIALES DE EJECUCIÓN ESTABLECIDAS": "Texto",
+    "UNIDAD ENCARGADA DEL SEGUIMIENTO Y EJECUCIÓN DEL CONTRATO": "UNA",
+    "CONSIDERACIÓN COMO INFRACCIÓN GRAVE": "Texto",
     "GASTOS POR DESISTIMIENTO O RENUNCIA": "Texto",
 }
-print(lines[40])
 
 for i in range(len(lines)):
     # Check if the block contains the word
     for dato in myDict:
         if dato in lines[i].upper() and not myDict[dato]:
-            if myDict2[dato] == "Valor":
-                resp = None
+            if dato == "CLASIFICACIÓN":
+                clasificacion = ["GRUPO", "SUBGRUPO", "CATEGORÍA"]
+                if all(word in lines[i + 1].upper() for word in clasificacion):
+                    possible_lines = lines[i + 1]
+                elif any("GRUPO" in line.upper() for line in lines[i + 1 : i + 5]):
+                    possible_lines = ".".join(lines[i + 1 : i + 5])
+                myDict[dato] = extract_info(possible_lines)
+                break
+            elif myDict2[dato] == "Valor":
+                myDict[dato] = extraer_valor_monetario(i)
+                break
             else:
-                print(dato)
                 split = lines[i].split(":")
                 if len(split) > 1:
                     resp = split[1]
                 else:
-                    resp = split[0]
+                    resp = ""
                 j = i + 1
-                pattern_no = re.compile(r"X\s*[NO,SI].*", re.IGNORECASE)
+                if (
+                    ":" not in lines[i]
+                    and lines[j] == lines[j].upper()
+                    and "NO" not in lines[j].upper()
+                ):
+                    j = j + 1
+                pattern_no = re.compile(r"X.*[NO,SI].*", re.IGNORECASE)
                 if re.match(pattern_no, lines[j]):
                     resp = lines[j].replace("X", "").strip()
                 elif re.match(pattern_no, lines[j + 1]):
                     resp = lines[j + 1].replace("X", "").strip()
                 else:
+                    if resp == "" and lines[j] == lines[j].upper():
+                        resp = lines[j]
+                        myDict[dato] = resp
+                        break
                     while lines[j] != lines[j].upper():
-                        print(lines[j + 2])
                         resp += " " + lines[j]
                         j += 1
-            myDict[dato] = resp
+                myDict[dato] = resp
+            break
 
 
 pprint.pprint(myDict)
